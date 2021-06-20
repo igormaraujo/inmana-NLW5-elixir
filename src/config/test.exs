43080973efa0e43e4940a671fcc2a3bf8fa032b6
igormaraujo <igoraraujo@ufpa.br>
2021-06-19 21:37:24 -0300

    feat: Send notification email with supplies that are about to expire

--- src/config/test.exs
+++ src/config/test.exs
@@ -12,6 +12,8 @@ config :inmana, Inmana.Repo,
   hostname: "inmana-nlw5-elixir_db_1",
   pool: Ecto.Adapters.SQL.Sandbox
 
+config :inmana, Inmana.Mailer, adapter: Bamboo.TestAdapter
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :inmana, InmanaWeb.Endpoint,
